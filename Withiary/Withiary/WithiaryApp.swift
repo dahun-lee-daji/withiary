ec1ac50abbb0896ea8afc8da6c62d79ce58c8c64
dahun-lee-daji <68788135+dahun-lee-daji@users.noreply.github.com>
2023-09-06 00:43:28 +0900

    init: 프로젝트 생성

---
+++ Withiary/Withiary/WithiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithiaryApp.swift
+//  Withiary
+//
+//  Created by 이다훈 on 2023/09/06.
+//
+
+import SwiftUI
+
+@main
+struct WithiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
